17a07b0f7256f4c07b5ff15d06aee4af48400dd3
Andreas Müller <schnitzeltony@gmail.com>
2022-06-03 16:23:43 +0200

    wm3000i: Set correct server IP address again
    Dangerous business to get rid of!!!
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- wm3000i/wmglobal.h
+++ wm3000i/wmglobal.h
@@ -18,8 +18,8 @@
 
 //#define FVWM 1
 
-//#define TheDevice "127.0.0.1"
-#define TheDevice "192.168.6.181"
+#define TheDevice "127.0.0.1"
+//#define TheDevice "192.168.6.181"
 
 // V1.00 setzt auf wm3000u V1.04
 // V1.01 zusätzliche anzeige lastpunkt relativ zu X kanal eingebaut
