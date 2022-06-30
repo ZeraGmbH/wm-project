aae0cc0227461e94ca8b03a81d7559088f807f41
Andreas Müller <schnitzeltony@gmail.com>
2022-06-30 08:32:14 +0200

    CBigLetter: Prettify / Q3Frame -> QFrame
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- wm-common/gui/bigletter.cpp
+++ wm-common/gui/bigletter.cpp
@@ -1,131 +1,128 @@
-// BigLetter implementation
-
 #include "bigletter.h"
-#include <qpainter.h>
-#include <qsize.h>
-#include <qfontmetrics.h>
-//Added by qt3to4:
+#include <QPainter>
+#include <QFontMetrics>
 #include <QResizeEvent>
-#include <Q3Frame>
 
-CBigLetter::CBigLetter(int s,QString d,QWidget *parent,const char* name,Qt::WFlags f)
-    :Q3Frame(parent,name,f)
+CBigLetter::CBigLetter(int s, QString d, QWidget *parent, const char* name, Qt::WFlags f)
+    : QFrame(parent, name, f)
 {
-    m_nStartSize=s;
+    m_nStartSize = s;
     actFontSize(m_nStartSize);
-    m_sDisplay=d;
+    m_sDisplay = d;
     setFrame(frame);
     formatChanged = false;
 }
 
-
-CBigLetter::CBigLetter(QWidget *parent,const char* name,Qt::WFlags f)
-    :Q3Frame(parent,name,f)
+CBigLetter::CBigLetter(QWidget *parent, const char* name, Qt::WFlags f)
+    : QFrame(parent, name, f)
 {
     setFrame(frame);
 }
 
 void CBigLetter::drawContents(QPainter* p)
 {
-    int lFlags=Qt::AlignVCenter;
-    if (m_nAlign==leftAlign)
-	lFlags=Qt::AlignLeft;
+    int lFlags = Qt::AlignVCenter;
+    if (m_nAlign == leftAlign)
+        lFlags |= Qt::AlignLeft;
     else
-	lFlags|=Qt::AlignRight;
-    p->drawText(rect(),lFlags,m_sDisplay);
-}    
-
+        lFlags |= Qt::AlignRight;
+    p->drawText(rect(), lFlags, m_sDisplay);
+}
 
 void CBigLetter::display(QString s)
 {
     formatChanged = (m_sDisplay.length() != s.length());
-    m_sDisplay=s;
+    m_sDisplay = s;
     this->repaint();
 }
-
 
 QString CBigLetter::isDisplay() const
 {
     return(m_sDisplay);
 }
 
-
 void CBigLetter::setStartSize(int s)
 {
     m_nStartSize = s;
     actFontSize(m_nStartSize);
 }    
- 
 
 int CBigLetter::getStartSize() const
 {
     return(m_nStartSize);
 }
 
-
 void CBigLetter::setAlignment(cAlign a)
 {
-     m_nAlign=a;
-}     
-
+    m_nAlign = a;
+}
 
 cAlign CBigLetter::getAlignment() const
 {
     return(m_nAlign);
 }
 
-
 void CBigLetter::setFrame(cFrame f)
 {
     m_nFrame=f;
     if (m_nFrame==frame)
-        setFrameStyle(Q3Frame::Box | Q3Frame::Plain);
-    else 
-        setFrameStyle(/*QFrame::MenuBarPanel |*/ Q3Frame::Plain);
-}    
-
+        setFrameStyle(QFrame::Box | QFrame::Plain);
+    else
+        setFrameStyle(QFrame::Plain);
+}
 
 cFrame CBigLetter::getFrame() const
 {
     return(m_nFrame);
 }
 
-
 bool CBigLetter::isFormatChanged()
 {
     return formatChanged;
 }
 
-
-void CBigLetter::resizeEvent ( QResizeEvent* r)
+void CBigLetter::resizeEvent (QResizeEvent* r)
 {
-    QSize ls=size();
+    QSize ls = size();
     actFontSize((int) ((1.0*ls.height())/1.6));
-    this->Q3Frame::resizeEvent(r);
+    this->QFrame::resizeEvent(r);
 }
 
+void CBigLetter::paintEvent(QPaintEvent *event)
+{
+    // Stolen from Q3Frame::paintEvent
+    QPainter paint(this);
+    if (!contentsRect().contains(event->rect())) {
+        paint.save();
+        paint.setClipRegion(event->region().intersected(frameRect()));
+        drawFrame(&paint);
+        paint.restore();
+    }
+    if (event->rect().intersects(contentsRect())) {
+        paint.setClipRegion(event->region().intersected(contentsRect()));
+        drawContents(&paint);
+    }
+}
 
 QSize CBigLetter::sizeHint() const 
 {
     QFont fo(font());
     fo.setPointSize(m_nStartSize); // empfohlene grösse ist immer die startgrösse
     QFontMetrics tfm(fo);
-    return(tfm.size(Qt::TextSingleLine,m_sDisplay));
+    return(tfm.size(Qt::TextSingleLine, m_sDisplay));
 }
-
 
 QSize CBigLetter::minimumSizeHint() const
 {
     QFont fo(font());
     fo.setPointSize(m_nStartSize);
     QFontMetrics tfm(fo);
-    return(tfm.size(Qt::TextSingleLine,m_sDisplay));
+    return(tfm.size(Qt::TextSingleLine, m_sDisplay));
 }
 
-
 void CBigLetter::actFontSize(int fs)
-{	
-    QFont fo( font());
+{
+    QFont fo(font());
     fo.setPointSize(fs);
     setFont(fo);
-}    
+}
--- wm-common/gui/bigletter.h
+++ wm-common/gui/bigletter.h
@@ -6,33 +6,24 @@
 #ifndef BIGLETTER_H
 #define BIGLETTER_H
 
-#include <qwidget.h>
-#include <q3frame.h>
-#include <qobject.h>
-#include <qstring.h>
-//Added by qt3to4:
+#include <QFrame>
 #include <QResizeEvent>
 
-//class QString;
-enum cAlign {rightAlign,leftAlign}; // rechtsbündig ist default
-enum cFrame {frame,noframe}; // mit rahmen ist default
+enum cAlign {rightAlign, leftAlign}; // rechtsbündig ist default
+enum cFrame {frame, noframe}; // mit rahmen ist default
    
-//class cAlign;
-//class cFrame;
-
-class CBigLetter:public Q3Frame
+class CBigLetter:public QFrame
 {
     Q_OBJECT
-    Q_PROPERTY( QString m_sDisplay READ isDisplay WRITE display)
-    Q_PROPERTY( int m_nStartSize READ getStartSize WRITE setStartSize)
-    Q_PROPERTY( cAlign m_nAlign READ getAlignment WRITE setAlignment)
-    Q_PROPERTY( cFrame m_nFrame READ getFrame WRITE setFrame) 
+    Q_PROPERTY(QString m_sDisplay READ isDisplay WRITE display)
+    Q_PROPERTY(int m_nStartSize READ getStartSize WRITE setStartSize)
+    Q_PROPERTY(cAlign m_nAlign READ getAlignment WRITE setAlignment)
+    Q_PROPERTY(cFrame m_nFrame READ getFrame WRITE setFrame)
     Q_ENUMS(cAlign)
     Q_ENUMS(cFrame)
-    
 public:
-    CBigLetter(int,QString,QWidget* parent=0,const char* name=0,Qt::WFlags f=0);
-    CBigLetter(QWidget* parent=0,const char* name=0,Qt::WFlags f=0);
+    CBigLetter(int, QString, QWidget* parent=0, const char* name=nullptr, Qt::WFlags f=0);
+    CBigLetter(QWidget* parent=0, const char* name=nullptr, Qt::WFlags f=0);
     void display(QString);
     QString isDisplay() const;
     void setStartSize(int);
@@ -42,22 +33,19 @@ public:
     void setFrame(cFrame);
     cFrame getFrame() const;
     bool isFormatChanged();
-    
 protected:    
-    virtual void resizeEvent ( QResizeEvent*);
-//    virtual void resize (int,int);
-    virtual void drawContents (QPainter*); // überschreibt drawcontents aus qframe 
-    virtual QSize sizeHint() const; //  gibt die optimale grösse zurück
-    virtual QSize minimumSizeHint() const; // gibt die minimale grösse zurück 
-//    virtual int heightForWidth(int) const;
-    
+    virtual void resizeEvent(QResizeEvent *) override;
+    void paintEvent(QPaintEvent *p) override;
+    virtual QSize sizeHint() const override; //  gibt die optimale grösse zurück
+    virtual QSize minimumSizeHint() const override; // gibt die minimale grösse zurück
 private:
+    void drawContents(QPainter *); // überschreibt drawcontents aus qframe
     void actFontSize(int);
     int m_nStartSize; // minimale grösse=startgrösse
     QString m_sDisplay;
     cAlign m_nAlign;
     cFrame m_nFrame;
     bool formatChanged;
-};    
+};
   
 #endif
