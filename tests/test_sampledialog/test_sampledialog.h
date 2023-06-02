fbedae41df05cf3ea9a802c968e0a7715473fd28
Wilfried Schunk <w.schunk@zera.de>
2023-06-02 08:18:49 +0200

    adds a simple sampleview (scope) to wm-project
    this displays the samples stored in given DSP memory ranges
    uses qpainter as qcharts is not avail. on qt4.8
    
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

---
+++ tests/test_sampledialog/test_sampledialog.h
@@ -0,0 +1,24 @@
+#ifndef TEST_SAMPLEDIALOG_H
+#define TEST_SAMPLEDIALOG_H
+
+#include <QtTest>
+#include <wmsampledialog.h>
+
+// add necessary includes here
+
+class test_sampledialog : public QObject
+{
+    Q_OBJECT
+
+public:
+    test_sampledialog();
+    ~test_sampledialog();
+
+private slots:
+    void test_properties();
+    void test_copyFloats();
+    void test_minMax();
+    void test_toHeight();
+};
+
+#endif // TEST_
--- tests/tests.pro
+++ tests/tests.pro
@@ -1,6 +1,7 @@
 TEMPLATE = subdirs
 
 SUBDIRS = \
+    test_sampledialog \
     test_settingschangetimer \
     test_sessionfilenamegen \
     test_sessionstreamer
