5afe4fe09d22b3ce513380f2956546c501376868
Wilfried Schunk <w.schunk@zera.de>
2023-07-19 10:07:42 +0200

    wm3000i: adds more abort possibility in phaseadjustment
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

--- wm3000u/wm3000u.cpp
+++ wm3000u/wm3000u.cpp
@@ -2066,6 +2066,12 @@ void cWM3000U::ActionHandler(int entryAHS)
         mCount--;
         mWmProgressDialog->setLabelText (QString("%1 %2 ...").arg(m_sJustText).arg(mCount));
         mWmProgressDialog->setValue3(mCount);
+        if (mWmProgressDialog->isAbort()) {
+            AHS = PhaseNodeMeasExec5;
+            N=4;
+            m_ActTimer->start(0,wm3000Continue);
+            break;
+        }
 
         if (mCount == 0)
         { // eingeschwungen
@@ -2105,6 +2111,13 @@ void cWM3000U::ActionHandler(int entryAHS)
         int i;
         ph0 = ActValues.PHIN;
         ph1 = ActValues.PHIX;
+        if (mWmProgressDialog->isAbort()) {
+            AHS = PhaseNodeMeasExec5;
+            N=4;
+            m_ActTimer->start(0,wm3000Continue);
+            break;
+        }
+
         switch (PhaseNodeMeasInfo->m_nJMode)
         {
         case adcNPhase:
@@ -2166,6 +2179,13 @@ void cWM3000U::ActionHandler(int entryAHS)
 
     case PhaseNodeMeasExec4:
     {
+        if (mWmProgressDialog->isAbort()) {
+            AHS = PhaseNodeMeasExec5;
+            N=4;
+            m_ActTimer->start(0,wm3000Continue);
+            break;
+        }
+
         switch (PhaseNodeMeasInfo->m_nJMode)
         {
         case adcNPhase:
