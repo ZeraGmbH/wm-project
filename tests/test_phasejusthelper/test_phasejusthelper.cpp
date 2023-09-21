fb270889d820721eb1aaa73d8b0bde9445d70200
Wilfried Schunk <w.schunk@zera.de>
2023-09-21 08:38:19 +0200

    phastjusthelpers adds log output
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

--- tests/test_phasejusthelper/test_phasejusthelper.cpp
+++ tests/test_phasejusthelper/test_phasejusthelper.cpp
@@ -2,6 +2,97 @@
 
 test_phasejusthelper::test_phasejusthelper()
 {
+
+}
+
+test_phasejusthelper::~test_phasejusthelper()
+{
+
+}
+
+void test_phasejusthelper::test_case1()
+{
+    PhastJustHelpers helper;
+    initTestValues();
+    helper.calculateMinMaxDiffValues(&values);
+    QCOMPARE( qFuzzyCompare(helper.getMinValue(),static_cast<float>(0.040)), true);
+    QCOMPARE( qFuzzyCompare(helper.getMaxValue(),static_cast<float>(1.72)), true);
+}
+
+void test_phasejusthelper::test_case2()
+{
+    PhastJustHelpers helper;
+    helper.calculateMinMaxDiffValues(&values2);
+    QCOMPARE( qFuzzyCompare(helper.getMinValue(),static_cast<float>(38.1)), true);
+    QCOMPARE( qFuzzyCompare(helper.getMaxValue(),static_cast<float>(38.9)), true);
+}
+
+void test_phasejusthelper::test_case3()
+{
+    PhastJustHelpers helper;
+    helper.calculateMinMaxDiffValues(&values);
+    helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values);
+    QCOMPARE(values.count(),19);
+}
+
+void test_phasejusthelper::test_case4()
+{
+    PhastJustHelpers helper;
+    helper.calculateMinMaxDiffValues(&values2);
+    helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values2);
+    QCOMPARE(values2.count(),19);
+
+}
+
+void test_phasejusthelper::test_case5()
+{
+    PhastJustHelpers helper;
+    helper.calculateMinMaxDiffValues(&values3);
+    helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values3);
+    QCOMPARE(values2.count(),19);
+    //QString log;
+    //log = helper.getLogString();
+}
+
+void test_phasejusthelper::test_case6()
+{
+    bool bVal;
+    initTestValues();
+    PhastJustHelpers helper;
+    helper.calculateMinMaxDiffValues(&values3);
+    bVal = helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values3);
+    QCOMPARE(bVal, true);
+    helper.calculateMinMaxDiffValues(&values3);
+    bVal = helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values3);
+    QCOMPARE(bVal, false);
+}
+
+void test_phasejusthelper::test_addLogString()
+{
+    PhastJustHelpers helper;
+    helper.addToLog("TEST");
+    helper.addToLog("TEST");
+    QString test ="TEST\nTEST\n";
+    QCOMPARE(helper.getLogString(),test);
+}
+
+void test_phasejusthelper::test_clearLogString()
+{
+    PhastJustHelpers helper;
+    helper.addToLog("TEST");
+    QString test ="TEST\n";
+    QCOMPARE(helper.getLogString(),test);
+    helper.clearLogStr();
+    test.clear();
+    QCOMPARE(helper.getLogString(),test);
+}
+
+void test_phasejusthelper::initTestValues()
+{
+    values.clear();
+    values2.clear();
+    values3.clear();
+
     values.append(0.041);
     values.append (0.042);
     values.append (0.040);
@@ -43,44 +134,27 @@ test_phasejusthelper::test_phasejusthelper()
     values2.append(38.7);
     values2.append(38.9);
     values2.append(38.8);
-}
 
-test_phasejusthelper::~test_phasejusthelper()
-{
-
-}
-
-void test_phasejusthelper::test_case1()
-{
-    PhastJustHelpers helper;
-    helper.calculateMinMaxDiffValues(&values);
-    QCOMPARE( qFuzzyCompare(helper.getMinValue(),static_cast<float>(0.040)), true);
-    QCOMPARE( qFuzzyCompare(helper.getMaxValue(),static_cast<float>(1.72)), true);
-}
-
-void test_phasejusthelper::test_case2()
-{
-    PhastJustHelpers helper;
-    helper.calculateMinMaxDiffValues(&values2);
-    QCOMPARE( qFuzzyCompare(helper.getMinValue(),static_cast<float>(38.1)), true);
-    QCOMPARE( qFuzzyCompare(helper.getMaxValue(),static_cast<float>(38.9)), true);
-}
-
-void test_phasejusthelper::test_case3()
-{
-    PhastJustHelpers helper;
-    helper.calculateMinMaxDiffValues(&values);
-    helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values);
-    QCOMPARE(values.count(),19);
-}
-
-void test_phasejusthelper::test_case4()
-{
-    PhastJustHelpers helper;
-    helper.calculateMinMaxDiffValues(&values2);
-    helper.deleteFaultyPhasenJustageItem(helper.getMeanValues(),helper.getDiffValue(),&values2);
-    QCOMPARE(values2.count(),19);
-
+    values3.append(0.0187967);
+    values3.append(0.0188513);
+    values3.append(0.0188513);
+    values3.append(0.0188513);
+    values3.append(0.0187967);
+    values3.append(0.0187967);
+    values3.append(0.018906);
+    values3.append(0.0188513);
+    values3.append(0.0188513);
+    values3.append(0.0188513);
+    values3.append(0.0188513);
+    values3.append(0.0187967);
+    values3.append(0.0188513);
+    values3.append(0.0187967);
+    values3.append(0.018906);
+    values3.append(0.0187967);
+    values3.append(0.0187967);
+    values3.append(0.0189879);
+    values3.append(0.0188513);
+    values3.append(1.70558);
 }
 
 
--- wm-common/just/phastjusthelpers.cpp
+++ wm-common/just/phastjusthelpers.cpp
@@ -13,8 +13,9 @@ PhastJustHelpers::PhastJustHelpers()
 
 }
 
-void PhastJustHelpers::deleteFaultyPhasenJustageItem(float mittel, float diff, QList<float> *anglesList)
+bool PhastJustHelpers::deleteFaultyPhasenJustageItem(float mittel, float diff, QList<float> *anglesList)
 {
+    bool retValue(false);
     float val, highTrigger, lowTrigger;
     if (diff > 0.4){
         highTrigger = (mittel + (diff*0.8));
@@ -23,26 +24,46 @@ void PhastJustHelpers::deleteFaultyPhasenJustageItem(float mittel, float diff, Q
             val = anglesList->at(i);
             if ((val > highTrigger) || (val < lowTrigger)){
                 anglesList->removeAt(i);
+                addToLog(QString("removed Item Nr.: %1 ; val: %2").arg(i).arg(val));
+                retValue = true;
             }
         }
     }
+    return retValue;
 }
 
-void PhastJustHelpers::calculateMinMaxDiffValues(QList<float> *angleList)
+void PhastJustHelpers::calculateMinMaxDiffValues(QList<float> *angleList, bool log)
 {
     qreal min(359.99), max(0.0), diff(0.0), value(0.0), ph0(0.0);
     for (int i = 0; i < angleList->count(); i++){
         value = angleList->at(i);
+        if (log) addToLog(QString("Ph0: %1").arg(value));
         if( min > value) min = value;
         if (max < value) max = value;
         ph0 -= value;
     }
     diff = (max - min) * 60.0;  // 1,6858° -> 1° 41' 9" -> 41,1516'
+    addToLog(QString("Diff: %1 Minuten").arg(diff));
     ph0 /= angleList->count();
     m_fMin = min;
     m_fMax = max;
     m_fDiff = diff / 60.0;
     m_fMean = ph0 *-1;
+}
+
+void PhastJustHelpers::addToLog(QString str)
+{
+    m_logString.append(str+"\n");
+}
+
+QString PhastJustHelpers::getLogString()
+{
+    return m_logString;
+}
+
+void PhastJustHelpers::clearLogStr()
+{
+    m_logString.clear();
 }
 
 float PhastJustHelpers::getMinValue()
