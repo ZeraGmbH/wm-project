24728292ee43c8851c953c07d1a162b28dc9c255
Wilfried Schunk <w.schunk@zera.de>
2024-11-20 12:18:22 +0100

    adds new class for command line parameter
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

---
+++ wm-common/settings/commandlineparameter.cpp
@@ -0,0 +1,52 @@
+#include "commandlineparameter.h"
+
+
+CommandLineParameter::CommandLineParameter()
+{
+
+}
+
+void CommandLineParameter::Parse(int argc, char *argv[])
+{
+    QString option;
+    for (int i = 1; i < argc; i++)
+    {
+        option = argv[i];
+        if (option == strJustage){
+            bJustage = true;
+            qDebug("Found option justage");
+        }
+        if (option == strConvent){
+            bConvent = true;
+            qDebug("Found option convent");
+        }
+        if (option == strDc){
+            bDc = true;
+            qDebug("Found option dc");
+        }
+        if (option == strNewSampleRates){
+            bNewSampleRates = true;
+            qDebug("Found option newsamplerates");
+        }
+        if(option.startsWith(strIPAdress)) {
+            IPAdress = option.replace(strIPAdress, "").trimmed();
+            qDebug("Found option ip address");
+        }
+        if (option == strPpsWatchDog){
+            bPpsWatchDog = true;
+            qDebug("Found option PPS Watch Dog");
+        }
+    }
+}
+
+QString CommandLineParameter::GetOptionString()
+{
+    QString option;
+    if(bJustage) option += "just ";
+    if(bConvent) option += "conv ";
+    if(bDc) option += "dc ";
+    if(bNewSampleRates) option += "newsr ";
+    if(bPpsWatchDog) option += "pwdt ";
+    if(strIPAdress != IPAdress) option += "ip";
+    return option;
+}
---
+++ wm-common/settings/commandlineparameter.h
@@ -0,0 +1,35 @@
+#ifndef COMMANDLINEPARAMETER_H
+#define COMMANDLINEPARAMETER_H
+
+#include <QString>
+
+class CommandLineParameter
+{
+public:
+    CommandLineParameter();
+    void Parse(int argc, char *argv[]);
+    QString GetOptionString();
+    bool GetJustage() {return bJustage;};
+    bool GetConvent() {return bConvent;};
+    bool GetDC() {return bDc;};
+    bool GetNewSampleRates() {return bNewSampleRates;};
+    bool GetPpsWatchDog() {return bPpsWatchDog;};
+    QString GetIpAdress() {return IPAdress;}
+private:
+    bool bJustage = false;
+    bool bConvent = false;
+    bool bDc = false;
+    bool bNewSampleRates = false;
+    bool bPpsWatchDog = false;
+    QString IPAdress = "127.0.0.1";
+
+    const QString strJustage = "-justage";
+    const QString strConvent = "-convent";
+    const QString strDc = "-dc";
+    const QString strNewSampleRates = "-newsamplerates";
+    const QString strIPAdress = "-ip";
+    const QString strPpsWatchDog = "-ppswatchdog";
+
+};
+
+#endif // COMMANDLINEPARAMETER_H
