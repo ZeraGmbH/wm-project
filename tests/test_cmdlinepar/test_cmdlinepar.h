599babee764ffa9491933f4c529838c1ef4f8340
Wilfried Schunk <w.schunk@zera.de>
2024-11-22 09:31:03 +0100

    adds test to commandlineparameter
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

---
+++ tests/test_cmdlinepar/test_cmdlinepar.h
@@ -0,0 +1,25 @@
+#ifndef TEST_CMDLINEPAR
+#define TEST_CMDLINEPAR
+
+#include <QTest>
+#include "commandlineparameter.h"
+
+class test_cmdlinepar : public QObject
+{
+	Q_OBJECT
+private:
+    void init(QString str);
+    char * helper(QString str);
+    int argc;
+    char *argv[2];
+private slots:
+    void test_numberone();
+    void test_empty();
+    void test_justage();
+    void test_convent();
+    void test_dc();
+    void test_sample();
+    void test_ppswdt();
+};
+
+#endif
