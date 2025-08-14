a31a84928d78db323cdfcc08c091c8828d759328
Wilfried Schunk <w.schunk@zera.de>
2025-08-14 15:10:53 +0200

    adds ratioWidget Screenshooter to wm3000i
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

--- wm3000i/confdialogbase.h
+++ wm3000i/confdialogbase.h
@@ -45,7 +45,8 @@ public slots:
     void screenshooterTriggered();
     void keyboardScreenshorTriggerd();
     void showRatio();
-
+    void takeScreenshoots();
+    void takeScreenshootFinished();
 signals:
     void SendConfDataSignal(cConfData*);
 
