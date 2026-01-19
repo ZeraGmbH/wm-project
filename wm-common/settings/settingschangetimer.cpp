23199a7031ce91d611a518c54c7ddb9ab77430a2
Wilfried Schunk <w.schunk@zera.de>
2026-01-19 10:08:51 +0100

    changes settingschangetimer to ensure storage of widget geometrie
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

--- wm-common/settings/settingschangetimer.cpp
+++ wm-common/settings/settingschangetimer.cpp
@@ -5,15 +5,23 @@ SettingsChangeTimer::SettingsChangeTimer(int geomTimerMs)
     m_settingsChangeTimer.setInterval(geomTimerMs);
     m_settingsChangeTimer.setSingleShot(false);
     connect(&m_settingsChangeTimer, SIGNAL(timeout()), this, SLOT(onGeomTimer()));
+    m_dontStop = false;
 }
 
-void SettingsChangeTimer::startDelayed()
+void SettingsChangeTimer::startDelayed(bool dontStop)
 {
     m_settingsChangeTimer.start();
+    m_dontStop = dontStop;
 }
 
 void SettingsChangeTimer::onGeomTimer()
 {
-    m_settingsChangeTimer.stop();
+    if (!m_dontStop)
+        m_settingsChangeTimer.stop();
+    emit sigWriteStreamForGeomChange();
+}
+
+void SettingsChangeTimer::onGeomTimer_()
+{
     emit sigWriteStreamForGeomChange();
 }
